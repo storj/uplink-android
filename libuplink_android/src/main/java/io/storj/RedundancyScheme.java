f9bf5c69f27f9f194cd6e5ca51f3f14997998e43
Kaloyan Raev <kaloyan@storj.io>
2019-09-25 01:09:49 +0300

    Builder pattern for BucketConfig, EncryptionParameters, RedundancyScheme

--- libuplink_android/src/main/java/io/storj/RedundancyScheme.java
+++ libuplink_android/src/main/java/io/storj/RedundancyScheme.java
@@ -4,16 +4,6 @@ public class RedundancyScheme {
 	
 	private io.storj.libuplink.mobile.RedundancyScheme scheme;
 	
-	public RedundancyScheme(RedundancyAlgorithm algorithm, short required, short repair, short success, short total, int shareSize) {
-		this.scheme = new io.storj.libuplink.mobile.RedundancyScheme();
-		this.scheme.setAlgorithm(algorithm.getValue());
-		this.scheme.setRequiredShares(required);
-		this.scheme.setRepairShares(repair);
-		this.scheme.setOptimalShares(success);
-		this.scheme.setTotalShares(total);
-		this.scheme.setShareSize(shareSize);
-	}
-	
 	public RedundancyAlgorithm getAlgorithm() {
 		return RedundancyAlgorithm.fromValue(this.scheme.getAlgorithm());
 	}
@@ -37,8 +27,64 @@ public class RedundancyScheme {
 	public int getShareSize() {
 		return this.scheme.getShareSize();
 	}
-	
+
 	io.storj.libuplink.mobile.RedundancyScheme internal() {
 		return scheme;
+	}
+
+	private RedundancyScheme(Builder builder) {
+		this.scheme = new io.storj.libuplink.mobile.RedundancyScheme();
+		if (builder.algorithm != null) {
+			this.scheme.setAlgorithm(builder.algorithm.getValue());
+		}
+		this.scheme.setRequiredShares(builder.required);
+		this.scheme.setRepairShares(builder.repair);
+		this.scheme.setOptimalShares(builder.success);
+		this.scheme.setTotalShares(builder.total);
+		this.scheme.setShareSize(builder.shareSize);
+	}
+
+	public static class Builder {
+
+		private RedundancyAlgorithm algorithm;
+		private short required;
+		private short repair;
+		private short success;
+		private short total;
+		private int shareSize;
+
+		public Builder setAlgorithm(RedundancyAlgorithm algorithm) {
+			this.algorithm = algorithm;
+			return this;
+		}
+
+		public Builder setRequiredShares(short count) {
+			this.required = count;
+			return this;
+		}
+
+		public Builder setRepairShares(short count) {
+			this.repair = count;
+			return this;
+		}
+
+		public Builder setSuccessShares(short count) {
+			this.success = count;
+			return this;
+		}
+
+		public Builder setTotalShares(short count) {
+			this.total = count;
+			return this;
+		}
+
+		public Builder setShareSize(int size) {
+			this.shareSize = size;
+			return this;
+		}
+
+		public RedundancyScheme build() {
+			return new RedundancyScheme(this);
+		}
 	}
 }
