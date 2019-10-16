6403cd9378c7d14cf4848f21067778141e12868b
Michal Niewrzal <michal@storj.io>
2019-10-16 10:40:25 +0200

    leave only javadoc

--- scripts/test-libuplink.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ueo pipefail
-
-# Might be easier way than -Pandroid.testInstrumentationRunnerArguments
-./gradlew connectedAndroidTest -Pandroid.testInstrumentationRunnerArguments.scope=$GATEWAY_0_SCOPE
